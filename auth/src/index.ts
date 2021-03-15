987f08db608f9c9fda9c9dc7afa82164690ca14d
Sri Hari Acha <srihari.acha@emirates.com>
2021-03-15 19:18:05 +0400

    mongo db implementation

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -23,6 +24,22 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log('Auth is listening on 3000');
-});
+const start = async () => {
+    console.log('Starting Mongo DB');
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('Connected to Mongo DB');
+    } catch (err) {
+        console.log(err);
+    }
+
+    app.listen(3000, () => {
+        console.log('Auth is listening on 3000');
+    });
+}
+
+start();
